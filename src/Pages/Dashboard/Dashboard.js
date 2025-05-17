154d7a92f91bfcc3f9756b5c1bd971ee4c82edcd
junayed <junayedmollick536@gmail.com>
2025-05-17 17:26:43 +0530

    added new page

---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DashboardHero from '../../components/Dashboard/DashboardHero/DashboardHero';
+
+const Dashboard = () => {
+  return (
+    <>
+
+    <DashboardHero/>
+      
+    </>
+  )
+}
+
+export default Dashboard;
---
+++ src/components/Dashboard/DashboardHero/DashboardHero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DashboardHero = () => {
+  return (
+    <>
+      
+    
+
+    </>
+  )
+}
+
+export default DashboardHero;
